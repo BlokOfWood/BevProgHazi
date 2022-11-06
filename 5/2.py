44818c299375abeb7acd42b933cdfbce979d0008
BlokOfWood <strikerelite@outlook.hu>
2022-11-06 19:26:46 +0100

    palindrom program now ignores punctuation and lower/upper diff

--- 5/2.py
+++ 5/2.py
@@ -1,5 +1,9 @@
+import string
+
 if __name__ == '__main__':
-    userInput = input("Please input a word, that you want to know about whether it's a palindrome: ").replace(' ', '')
+    userInput = input("Please input a word, that you want to know about whether it's a palindrome: ").replace(' ', '') 
+    for char in string.punctuation:
+        userInput = userInput.replace(char, '')
 
     multiCharacterLetters = ['cs', 'dz', 'dzs', 'gy', 'ly', 'sz', 'ty', 'zs']
     reversed = userInput[::-1]
@@ -7,7 +11,7 @@ if __name__ == '__main__':
     for letter in multiCharacterLetters:
         reversed = reversed.replace(letter[::-1], letter)
 
-    if userInput == reversed:
+    if userInput.lower() == reversed.lower():
         print("The word is a palindrome.")
     else:
         print("The word isn't a palindrome.")
