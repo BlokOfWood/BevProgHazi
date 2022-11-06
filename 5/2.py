31741109e5de8adf9e0b1d9674b6c3eec974f733
BlokOfWood <strikerelite@outlook.hu>
2022-11-06 19:19:28 +0100

    palindrome program now handles multi character letters

--- 5/2.py
+++ 5/2.py
@@ -1,6 +1,13 @@
 if __name__ == '__main__':
     userInput = input("Please input a word, that you want to know about whether it's a palindrome: ").replace(' ', '')
-    if userInput == userInput[::-1]:
+
+    multiCharacterLetters = ['cs', 'dz', 'dzs', 'gy', 'ly', 'sz', 'ty', 'zs']
+    reversed = userInput[::-1]
+    
+    for letter in multiCharacterLetters:
+        reversed = reversed.replace(letter[::-1], letter)
+
+    if userInput == reversed:
         print("The word is a palindrome.")
     else:
         print("The word isn't a palindrome.")
